2de50ec09fe642dae0a54554ab7de75dab8a4006
Tony Trinh <tony19@gmail.com>
2016-04-07 12:41:37 -0500

    :zap: Fix json params in curl

--- triggertestapp.sh
+++ triggertestapp.sh
@@ -1,7 +1,5 @@
 #!/bin/sh -e
-echo SHELL=$SHELL
-pwd
-echo sourcing gradle.properties
+
 . ./gradle.properties
 
 token=${CIRCLE_TOKEN?}
@@ -10,23 +8,23 @@ project=logback-test-app
 
 curl -X POST  --header "Content-Type: application/json" -d '{
     "build_parameters": {
-      "LOGBACK_ANDROID_VERSION": "$logbackAndroidVersion",
-      "SLF4J_VERSION": "$slf4jVersion",
-      "CIRCLE_PROJECT_USERNAME": "$CIRCLE_PROJECT_USERNAME",
-      "CIRCLE_PROJECT_REPONAME": "$CIRCLE_PROJECT_REPONAME",
-      "CIRCLE_BRANCH": "$CIRCLE_BRANCH",
-      "CIRCLE_TAG": "$CIRCLE_TAG",
-      "CIRCLE_SHA1": "$CIRCLE_SHA1",
-      "CIRCLE_REPOSITORY_URL": "$CIRCLE_REPOSITORY_URL",
-      "CIRCLE_COMPARE_URL": "$CIRCLE_COMPARE_URL",
-      "CIRCLE_BUILD_URL": "$CIRCLE_BUILD_URL",
-      "CIRCLE_BUILD_NUM": "$CIRCLE_BUILD_NUM",
-      "CIRCLE_PREVIOUS_BUILD_NUM": "$CIRCLE_PREVIOUS_BUILD_NUM",
-      "CI_PULL_REQUESTS": "$CI_PULL_REQUESTS",
-      "CI_PULL_REQUEST": "$CI_PULL_REQUEST",
-      "CIRCLE_USERNAME": "$CIRCLE_USERNAME",
-      "CIRCLE_PR_USERNAME": "$CIRCLE_PR_USERNAME",
-      "CIRCLE_PR_REPONAME": "$CIRCLE_PR_REPONAME",
-      "CIRCLE_PR_NUMBER": "$CIRCLE_PR_NUMBER"
+      "LOGBACK_ANDROID_VERSION": "'$logbackAndroidVersion'",
+      "SLF4J_VERSION": "'$slf4jVersion'",
+      "CIRCLE_PROJECT_USERNAME": "'$CIRCLE_PROJECT_USERNAME'",
+      "CIRCLE_PROJECT_REPONAME": "'$CIRCLE_PROJECT_REPONAME'",
+      "CIRCLE_BRANCH": "'$CIRCLE_BRANCH'",
+      "CIRCLE_TAG": "'$CIRCLE_TAG'",
+      "CIRCLE_SHA1": "'$CIRCLE_SHA1'",
+      "CIRCLE_REPOSITORY_URL": "'$CIRCLE_REPOSITORY_URL'",
+      "CIRCLE_COMPARE_URL": "'$CIRCLE_COMPARE_URL'",
+      "CIRCLE_BUILD_URL": "'$CIRCLE_BUILD_URL'",
+      "CIRCLE_BUILD_NUM": "'$CIRCLE_BUILD_NUM'",
+      "CIRCLE_PREVIOUS_BUILD_NUM": "'$CIRCLE_PREVIOUS_BUILD_NUM'",
+      "CI_PULL_REQUESTS": "'$CI_PULL_REQUESTS'",
+      "CI_PULL_REQUEST": "'$CI_PULL_REQUEST'",
+      "CIRCLE_USERNAME": "'$CIRCLE_USERNAME'",
+      "CIRCLE_PR_USERNAME": "'$CIRCLE_PR_USERNAME'",
+      "CIRCLE_PR_REPONAME": "'$CIRCLE_PR_REPONAME'",
+      "CIRCLE_PR_NUMBER": "'$CIRCLE_PR_NUMBER'"
     }
   }' https://circleci.com/api/v1/project/${username}/${project}?circle-token=${token}
